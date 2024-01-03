e0f3ffb78b6a3195700973183ad4efb6ab07b63f
Maksim <fors.horiz@gmail.com>
2024-01-03 11:22:54 +0300

    [+] linux

--- src/main.cpp
+++ src/main.cpp
@@ -35,11 +35,16 @@ int main(void)
         0.0f, 0.5f,
         0.5f, -0.5f
     };
-
+    
     unsigned int buffer = 0;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glBufferData(GL_ARRAY_BUFFER, positions.size() * sizeof(float), positions.data(), GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
